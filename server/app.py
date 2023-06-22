22c223e0334a68d97fb1ecc54e95f71d1d9054a6
Patricia <wambuipatricia3@gmail.com>
2023-06-22 22:54:14 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -44,9 +44,47 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+
+        record=Plant.query.filter_by(id=id).first()
+        if record is None:
+            return {"error":"Plant not found"},404
+        # for attr in request.form:
+        #     setattr(record,attr,request.form[attr])
+
+        # db.session.add(record)
+        record.is_in_stock =False
+        db.session.commit()   
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response 
+    
+
+    def delete(self,id):
+
+        record = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict={"message":"Plant is not in stock"}
+
+        response = make_response(
+            jsonify(response_dict),204
+        )
+
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
